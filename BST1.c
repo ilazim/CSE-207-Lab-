6aae2240a6a034b68f70049cdb553d386980f993
Inteser Lazim <inteser.lazim.cse@ulab.edu.bd>
2019-09-03 04:08:29 +0600

    Create BST1.c

---
+++ BST1.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    struct node* left;
+    struct node* right;
+    int data;
+};
+struct node* start = NULL;
+struct node* newNode(int x)
+{
+    struct node *mem;
+    mem = (struct node*)malloc(sizeof(struct node));
+    mem->data=x;
+    mem->left=mem->right = NULL;
+    return mem;
+}
+struct node* Insert (struct node* start, int data)
+{
+    if (start==NULL)
+    {
+        start=newNode(data);
+    }
+    else if(data<= start->data)
+    {
+        start->left = Insert(start->left,data);
+    }
+    else
+    {
+        start->right = Insert(start->right,data);
+    }
+    return start;
+}
+int find(struct node* start,int data)
+{
+    if(start == NULL)
+    {
+        return 0;
+    }
+    else if(start->data == data)
+    {
+        return 1;
+    }
+    else if(data <= start->data)
+    {
+        return find(start->left,data);
+    }
+    else
+    {
+        return find(start->right,data);
+    }
+}
+int main()
+{
+    int test;
+    printf("enter a number to find for:");
+    scanf("%d", &test);
+    start = Insert(start,1);
+    start = Insert(start,5);
+    start = Insert(start,2);
+    start = Insert(start,7);
+    start = Insert(start,8);
+    start = Insert(start,55);
+    if(find(start, test)==1)
+        printf("%d is in the tree.", test);
+    else
+        printf("%d is not in the tree.", test);
+    return 0;
+}
