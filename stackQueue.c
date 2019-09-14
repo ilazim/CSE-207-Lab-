8ebb03929229349aa5b3b61054e0bf4fa12ad57f
Inteser Lazim <inteser.lazim.cse@ulab.edu.bd>
2019-09-15 03:12:34 +0600

    Update stackQueue.c

--- stackQueue.c
+++ stackQueue.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define MAXSIZE 10
-
 int front1 = 0,
     front2 = 0,
     rear1 = -1,
     rear2 = -1;
-
 int data[MAXSIZE];
 int temp[MAXSIZE];
-
 void push(int x)
 {
     if (rear1>=MAXSIZE)
@@ -19,37 +16,25 @@ void push(int x)
       rear1++;
       data[rear1] = x;
     }
-
 }
 int dequeue()
 {
     int a = data[front1];
-   // printf("a=%d\n", a);
-
     front1++;
     return a;
 }
-
 void transfer()
 {
     int x;
-    //int y = rear1-1;
-   // printf("%d\n", y);
     for (x=rear1; x>=0; x--)
     {
         rear2++;
         temp[x] = dequeue();
-       // printf("x=%d\n", x);
-        //printf("temp,x=%d\n", temp[x]);
-
     }
-
 }
 int pop()
 {
     int b = temp[front2];
-       // printf("%d::", front2);
-
     front2++;
     return b;
 }
@@ -58,14 +43,9 @@ int main()
     push(5);
     push(6);
     push(7);
-   // printf("%d\n", rear1);
-
     transfer();
     printf("%d", pop());
     printf("%d", pop());
     printf("%d", pop());
-
-
-
     return 0;
 }
